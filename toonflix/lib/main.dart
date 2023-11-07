bd6e41c463076f437d47f6d6187c6ff6a2a5d5ff
ketchup0211 <dlcka1111@naver.com>
2023-11-07 19:03:13 +0900

    Flutter Basic function

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 10,
+          centerTitle: false,
+          backgroundColor: Colors.deepPurple,
+          title: const Text(
+            'Hello flutter!',
+          ),
+        ),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
