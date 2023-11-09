e1718f30bdcf55ef45e155b12429bc6f767a55e1
ketchup0211 <dlcka1111@naver.com>
2023-11-10 05:29:16 +0900

    Flutter API request and Fetch and convert

---
+++ webtoon/lib/models/webtoon_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WebtoonModel {
+  final String id, title, thumb;
+
+  WebtoonModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        thumb = json['thumb'];
+}
