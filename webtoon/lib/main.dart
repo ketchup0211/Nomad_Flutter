b1bb5bb1963595b4e064da1eeadb5073b970f6d6
ketchup0211 <dlcka1111@naver.com>
2023-11-10 04:04:28 +0900

    Flutter Webtoon App start

---
+++ webtoon/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:webtoon/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
